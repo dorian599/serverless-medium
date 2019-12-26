9fc6c3c22978d28d93438b079136f0093904878f
Dorian Machado <dorian599@gmail.com>
2019-12-26 13:01:41 -0300

    Lambda layers medium post

---
+++ test-lambda-layers/my-app/src/handler.py
@@ -0,0 +1,19 @@
+import json
+from jsonpath_rw import jsonpath, parse
+
+def hello(event, context):
+    body = {
+        "message": "Python Function executed successfully!"
+    }
+
+    jsonpath.auto_id_field = 'id'
+    data = [match.value for match in parse('foo[*].id').find({'foo': [{'id': 'bizzle'}, {'baz': 3}]})]
+
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body),
+        "data": json.dumps(data)
+    }
+
+    return response
